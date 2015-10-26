99b2feb87774692b1d089c353e89c4cef95d10da
Lubomir Chorbadjiev <lchorbadjiev@setelis.com>
2015-10-26 21:00:34 +0200

    small changes in hilbert curve...

--- 13/hilbert.cc
+++ 13/hilbert.cc
@@ -10,13 +10,13 @@ void a(int i, int h) {
 	if(i<=0) {
 		return;
 	}
-	d(i-1, h/2);
-	cout << '-' << h/2 << ' ' << 0 << ' ' << "rlineto" << endl;
-	a(i-1, h/2);
-	cout << 0 << ' ' << '-' << h/2 << ' ' << "rlineto" << endl;
-	a(i-1, h/2);
-	cout << h/2 << ' ' << 0 << ' ' << "rlineto" << endl;
-	b(i-1, h/2);
+	d(i-1, h);
+	cout << '-' << h << ' ' << 0 << ' ' << "rlineto" << endl;
+	a(i-1, h);
+	cout << 0 << ' ' << '-' << h << ' ' << "rlineto" << endl;
+	a(i-1, h);
+	cout << h << ' ' << 0 << ' ' << "rlineto" << endl;
+	b(i-1, h);
 	
 }
 
@@ -24,48 +24,54 @@ void b(int i, int h) {
 	if(i<=0) {
 		return;
 	}
-	c(i-1, h/2);
-	cout << 0 << ' ' << h/2 << ' ' << "rlineto" << endl;
-	b(i-1, h/2);
-	cout << h/2 << ' ' << 0 << ' ' << "rlineto" << endl;
-	b(i-1, h/2);
-	cout << 0 << ' ' << '-' << h/2 << ' ' << "rlineto" << endl;
-	a(i-1, h/2);
+	c(i-1, h);
+	cout << 0 << ' ' << h << ' ' << "rlineto" << endl;
+	b(i-1, h);
+	cout << h << ' ' << 0 << ' ' << "rlineto" << endl;
+	b(i-1, h);
+	cout << 0 << ' ' << '-' << h << ' ' << "rlineto" << endl;
+	a(i-1, h);
 }
 
 void c(int i, int h) {
 	if(i<=0) {
 		return;
 	}
-	b(i-1, h/2);
-	cout << h/2 << ' ' << 0 << ' ' << "rlineto" << endl;
-	c(i-1, h/2);
-	cout << 0 << ' ' << h/2 << ' ' << "rlineto" << endl;
-	c(i-1, h/2);
-	cout << '-' << h/2 << ' ' << 0 << ' ' << "rlineto" << endl;
-	d(i-1, h/2);
+	b(i-1, h);
+	cout << h << ' ' << 0 << ' ' << "rlineto" << endl;
+	c(i-1, h);
+	cout << 0 << ' ' << h << ' ' << "rlineto" << endl;
+	c(i-1, h);
+	cout << '-' << h << ' ' << 0 << ' ' << "rlineto" << endl;
+	d(i-1, h);
 }
 
 void d(int i, int h) {
 	if(i<=0) {
 		return;
 	}
-	a(i-1, h/2);
-	cout << 0 << ' ' << '-' << h/2 << ' ' << "rlineto" << endl;
-	d(i-1, h/2);
-	cout << '-' << h/2 << ' ' << 0 << ' ' << "rlineto" << endl;
-	d(i-1, h/2);
-	cout << 0 << ' ' << h/2 << ' ' << "rlineto" << endl;
-	c(i-1, h/2);
+	a(i-1, h);
+	cout << 0 << ' ' << '-' << h << ' ' << "rlineto" << endl;
+	d(i-1, h);
+	cout << '-' << h << ' ' << 0 << ' ' << "rlineto" << endl;
+	d(i-1, h);
+	cout << 0 << ' ' << h << ' ' << "rlineto" << endl;
+	c(i-1, h);
 }
 
 
 
 int main() {
 	cout << "newpath" << endl;
+
 	cout << "520 520 moveto" << endl;	
-	// cout << "10 10 moveto" << endl;
-	a(2, 128);
+	a(1, 512);
+
+	cout << "520 520 moveto" << endl;	
+	a(3, 512/8);
+	
+	cout << "520 530 moveto" << endl;
+	cout << "-512 0 rlineto" << endl;
 	cout << "stroke" << endl;
 	cout << "showpage" << endl;
 	
