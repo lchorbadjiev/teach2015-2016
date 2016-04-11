44030aabe37fa5c14435856efb0a989b7efc0e2c
Emil Gotsev <egotsev@gmail.com>
2016-04-11 22:49:59 +0300

    Added abstract superclass Atom

--- 2016-04-13-WaterSynchronizationInit/src/org/elsys/water/Hydrogen.java
+++ 2016-04-13-WaterSynchronizationInit/src/org/elsys/water/Hydrogen.java
@@ -1,32 +1,24 @@
 package org.elsys.water;
 
-public class Hydrogen extends Thread {
+public class Hydrogen extends Atom implements Runnable {
 
-	private final WaterConstructor constructor;
-
-	private final int hydrogenId;
-
-	public Hydrogen(int id, WaterConstructor constructor) {
-		this.hydrogenId = id;
-		this.constructor = constructor;
+	public Hydrogen(int atomId, WaterConstructor constructor) {
+		super(atomId, constructor);
 	}
 
 	@Override
 	public void run() {
 		System.out.println(this + " created");
 		try {
-			constructor.proceedHydrogen(this);
+			getConstructor().proceedHydrogen(this);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		System.out.println("Water molecule created with " + this);
 	}
 
-	public String toString() {
-		return "Hydrogen-" + hydrogenId;
-	}
-
-	public void bond() {
-		System.out.println(this + " bonded");
+	@Override
+	public String getFullName() {
+		return "Hydrogen";
 	}
 }
